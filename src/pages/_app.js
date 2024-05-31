19a10b50b2de7a6860b83bc76033cf87411f9c15
Stephin <stephinreji123@gmail.com>
2024-05-31 15:13:17 +0530

    Added meta tags

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,23 @@
 // pages/_app.js
-
+import Head from 'next/head';
 import '../styles/global.css';
 
+
 function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <Head>
+                <title>Formify AI - Generate Forms with AI</title>
+                <meta name="description" content="Formify AI helps you generate forms using AI. Create user-friendly and meaningful forms in just a few steps." />
+                <meta property="og:title" content="Formify AI - Generate Forms with AI" />
+                <meta property="og:description" content="Formify AI helps you generate forms using AI. Create user-friendly and meaningful forms in just a few steps." />
+                <meta property="og:url" content="formifyai.vercel.app" />
+                <meta name="twitter:title" content="Formify AI - Generate Forms with AI" />
+                <meta name="twitter:description" content="Formify AI helps you generate forms using AI. Create user-friendly and meaningful forms in just a few steps." />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
 }
 
 export default MyApp;
