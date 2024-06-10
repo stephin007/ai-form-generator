5be559eb300dfddf9e7be0267c0cff93f1f151b7
Stephin <stephinreji123@gmail.com>
2024-06-10 13:24:50 +0530

    Added speed analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,29 +1,45 @@
 // pages/_app.js
-import Head from 'next/head';
-import { Analytics } from '@vercel/analytics/react';
-import '../styles/global.css';
+import Head from "next/head";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import "../styles/global.css";
 
-import { AuthProvider } from '../../AuthContext';
-
+import { AuthProvider } from "../../AuthContext";
 
 function MyApp({ Component, pageProps }) {
-    return (
-        <>
-        <AuthProvider>
-            <Head>
-                <title>Formify AI - Generate Forms with AI</title>
-                <meta name="description" content="Formify AI helps you generate forms using AI. Create user-friendly and meaningful forms in just a few steps." />
-                <meta property="og:title" content="Formify AI - Generate Forms with AI" />
-                <meta property="og:description" content="Formify AI helps you generate forms using AI. Create user-friendly and meaningful forms in just a few steps." />
-                <meta property="og:url" content="formifyai.vercel.app" />
-                <meta name="twitter:title" content="Formify AI - Generate Forms with AI" />
-                <meta name="twitter:description" content="Formify AI helps you generate forms using AI. Create user-friendly and meaningful forms in just a few steps." />
-            </Head>
-            <Component {...pageProps} />
-            <Analytics />
-            </AuthProvider>
-        </>
-    );
+  return (
+    <>
+      <AuthProvider>
+        <Head>
+          <title>Formify AI - Generate Forms with AI</title>
+          <meta
+            name="description"
+            content="Formify AI helps you generate forms using AI. Create user-friendly and meaningful forms in just a few steps."
+          />
+          <meta
+            property="og:title"
+            content="Formify AI - Generate Forms with AI"
+          />
+          <meta
+            property="og:description"
+            content="Formify AI helps you generate forms using AI. Create user-friendly and meaningful forms in just a few steps."
+          />
+          <meta property="og:url" content="formifyai.vercel.app" />
+          <meta
+            name="twitter:title"
+            content="Formify AI - Generate Forms with AI"
+          />
+          <meta
+            name="twitter:description"
+            content="Formify AI helps you generate forms using AI. Create user-friendly and meaningful forms in just a few steps."
+          />
+        </Head>
+        <Component {...pageProps} />
+        <Analytics />
+        <SpeedInsights />
+      </AuthProvider>
+    </>
+  );
 }
 
 export default MyApp;
